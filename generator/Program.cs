7a9ab4d6faf6c56c29d7377a251d601acb7b937e
vbatushkov <vlad.batushkov@agoda.com>
2019-10-12 23:00:15 +0700

    flights schedule generator draft

---
+++ generator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FlightsScheduleGenerator
+{
+    class Program
+    {
+        private const string path = "../data/schedule.csv";
+
+        static void Main(string[] args)
+        {
+            using(var writer = new StreamWriter(path))
+            {
+                writer.WriteAsync($"Nothing yet");
+            }
+            Console.WriteLine($"Flights schedule generated!");
+        }
+    }
+}
