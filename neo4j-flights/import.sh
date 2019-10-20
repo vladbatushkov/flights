6aef624f0a7b022e32d1d0fbc3c00923bd946d14
vbatushkov <vlad.batushkov@agoda.com>
2019-10-20 19:59:35 +0700

    remove headers

--- neo4j-flights/import.sh
+++ neo4j-flights/import.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/var/lib/neo4j/bin/neo4j-admin import --database=flights.db --mode=csv --nodes:Airline=/var/lib/neo4j/import/airlines.csv --nodes:Airport=/var/lib/neo4j/import/airports.csv --relationships:FLIES_TO=/var/lib/neo4j/import/routes.csv && chmod -R 777 /data && chmod -R 777 /logs
+/var/lib/neo4j/bin/neo4j-admin import --database=flights.db --mode=csv --nodes:Airline=/var/lib/neo4j/import/airlines.csv --nodes:Airport=/var/lib/neo4j/import/airports.csv --relationships:FLIES_TO=/var/lib/neo4j/import/routes.csv --ignore-missing-nodes && chmod -R 777 /data && chmod -R 777 /logs
