2467e8e4fe6d4a8f06117cd9f93a915a230adf73
vbatushkov <vlad.batushkov@agoda.com>
2019-10-20 15:20:00 +0700

    2 docker images

--- neo4j-flights/import.sh
+++ neo4j-flights/import.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# do not run init script at each container strat but only at the first start
-/var/lib/neo4j/bin/neo4j-admin import --database=flights.db --mode=csv --nodes:Airline=/var/lib/neo4j/import/airlines_headers.csv,/var/lib/neo4j/import/airlines.csv
+/var/lib/neo4j/bin/neo4j-admin import --database=flights.db --mode=csv --nodes:Airline=/var/lib/neo4j/import/airlines_headers.csv,/var/lib/neo4j/import/airlines.csv && chmod -R 777 /data && chmod -R 777 /logs
