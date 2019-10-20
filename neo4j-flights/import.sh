18a364797cb0a581e87af999503d4f6ec72f2ecb
vbatushkov <vlad.batushkov@agoda.com>
2019-10-20 18:56:11 +0700

    airports

--- neo4j-flights/import.sh
+++ neo4j-flights/import.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-/var/lib/neo4j/bin/neo4j-admin import --database=flights.db --mode=csv --nodes:Airline=/var/lib/neo4j/import/airlines_headers.csv,/var/lib/neo4j/import/airlines.csv && chmod -R 777 /data && chmod -R 777 /logs
+/var/lib/neo4j/bin/neo4j-admin import \
+--database=flights.db \
+--mode=csv \
+--nodes:Airline=/var/lib/neo4j/import/airlines_headers.csv,/var/lib/neo4j/import/airlines.csv \
+--nodes:Airport=/var/lib/neo4j/import/airports_headers.csv,/var/lib/neo4j/import/airports.csv \
+&& chmod -R 777 /data && chmod -R 777 /logs
