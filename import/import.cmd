356d43804a3d948320ab0487f0fbd2de48c94b4d
vbatushkov <vlad.batushkov@agoda.com>
2019-10-20 19:47:14 +0700

    routes

--- import/import.cmd
+++
@@ -1,7 +0,0 @@
-neo4j-admin import \
---mode=csv \
---database=flights.db \
---report-file=flights.report \
---nodes:Airline ../import/airlines_headers.csv,../import/airlines.csv \
---ignore-duplicate-nodes=true \
---ignore-missing-nodes=true
