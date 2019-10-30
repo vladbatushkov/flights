23fef88ea553f16daf37f219d9d24ce6776ab93c
Vlad Batushkov <Vlad.Batushkov@agoda.com>
2019-10-31 02:10:31 +0700

    FlightsSearch draft

---
+++ grand-stack-app/api/src/resolvers.js
@@ -0,0 +1,11 @@
+import { neo4jgraphql } from "neo4j-graphql-js";
+
+const resolvers = {
+//   Query: {
+//     FlighsSearch(object, params, ctx, resolveInfo) {
+//       var result = neo4jgraphql(object, params, ctx, resolveInfo);
+//       console.log(result);
+//       return result;
+//     }
+//   }
+};
