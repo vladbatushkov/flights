65f31a98a819024bedeafca7f65cb9fc91a894a2
Vlad Batushkov <Vlad.Batushkov@agoda.com>
2020-01-04 23:25:27 +0700

    simplify query

--- neo4j-flights/docker-run.cmd
+++ neo4j-flights/docker-run.cmd
@@ -3,3 +3,5 @@ docker run -p7474:7474 -p7473:7473 -p7687:7687 vladbatushkov/neo4j-flights:lates
 
 --docker exec -it NAME bash
 --ls -l --block-size=M
+
+--docker exec neo2 /var/lib/neo4j/bin/cypher-shell -u neo4j -p test 'CREATE INDEX ON :Airport(country);'
